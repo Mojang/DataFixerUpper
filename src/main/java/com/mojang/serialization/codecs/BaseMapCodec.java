d1813341db5d8015eba275e2c262094bf0a31ed2
RainWarrior <rainwarrior@fry.su>
2020-05-12 09:39:56 +0200

    Use Stream.reduce in BaseMapCodec

--- src/main/java/com/mojang/serialization/codecs/BaseMapCodec.java
+++ src/main/java/com/mojang/serialization/codecs/BaseMapCodec.java
@@ -14,7 +14,6 @@ import com.mojang.serialization.MapLike;
 import com.mojang.serialization.RecordBuilder;
 
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 public interface BaseMapCodec<K, V> {
 
@@ -25,25 +24,28 @@ public interface BaseMapCodec<K, V> {
     default <T> DataResult<Map<K, V>> decode(final DynamicOps<T> ops, final MapLike<T> input) {
         final ImmutableMap.Builder<K, V> read = ImmutableMap.builder();
         final ImmutableList.Builder<Pair<T, T>> failed = ImmutableList.builder();
-        final AtomicReference<DataResult<Unit>> result = new AtomicReference<>(DataResult.success(Unit.INSTANCE, Lifecycle.stable()));
 
-        input.entries().forEach(pair -> {
-            final DataResult<K> k = keyCodec().parse(ops, pair.getFirst());
-            final DataResult<V> v = elementCodec().parse(ops, pair.getSecond());
+        final DataResult<Unit> result = input.entries().reduce(
+            DataResult.success(Unit.INSTANCE, Lifecycle.stable()),
+            (r, pair) -> {
+                final DataResult<K> k = keyCodec().parse(ops, pair.getFirst());
+                final DataResult<V> v = elementCodec().parse(ops, pair.getSecond());
 
-            final DataResult<Pair<K, V>> entry = k.apply2stable(Pair::of, v);
-            entry.error().ifPresent(e -> failed.add(pair));
+                final DataResult<Pair<K, V>> entry = k.apply2stable(Pair::of, v);
+                entry.error().ifPresent(e -> failed.add(pair));
 
-            result.set(result.get().apply2stable((u, p) -> {
-                read.put(p.getFirst(), p.getSecond());
-                return u;
-            }, entry));
-        });
+                return r.apply2stable((u, p) -> {
+                    read.put(p.getFirst(), p.getSecond());
+                    return u;
+                }, entry);
+            },
+            (r1, r2) -> r1.apply2stable((u1, u2) -> u1, r2)
+        );
 
         final Map<K, V> elements = read.build();
         final T errors = ops.createMap(failed.build().stream());
 
-        return result.get().map(unit -> elements).setPartial(elements).mapError(e -> e + " missed input: " + errors);
+        return result.map(unit -> elements).setPartial(elements).mapError(e -> e + " missed input: " + errors);
     }
 
     default <T> RecordBuilder<T> encode(final Map<K, V> input, final DynamicOps<T> ops, final RecordBuilder<T> prefix) {
