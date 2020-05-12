fdd15419ed65e636003edecf9c0d4adf5e4a325a
RainWarrior <rainwarrior@fry.su>
2020-05-12 16:52:26 +0200

    Added roundrip codec test

--- src/main/java/com/mojang/serialization/DataResult.java
+++ src/main/java/com/mojang/serialization/DataResult.java
@@ -54,6 +54,10 @@ public class DataResult<R> implements App<DataResult.Mu, R> {
         return new DataResult<>(Either.right(new PartialResult<>(message, Optional.empty())), lifecycle);
     }
 
+    public static <K, V> Function<K, DataResult<V>> partialGet(final Function<K, V> partialGet, final Supplier<String> errorPrefix) {
+        return name -> Optional.ofNullable(partialGet.apply(name)).map(DataResult::success).orElseGet(() -> error(errorPrefix.get() + name));
+    }
+
     private static <R> DataResult<R> create(final Either<R, PartialResult<R>> result, final Lifecycle lifecycle) {
         return new DataResult<>(result, lifecycle);
     }
