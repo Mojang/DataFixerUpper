4751094354ef08238165ec9d8613d81590039025
Gegy <gegy.dev@gmail.com>
2023-02-20 17:29:36 +0100

    Allow optimisation rules to be run within fold algebra

--- src/main/java/com/mojang/datafixers/functions/Fold.java
+++ src/main/java/com/mojang/datafixers/functions/Fold.java
@@ -8,14 +8,18 @@ import com.mojang.datafixers.RewriteResult;
 import com.mojang.datafixers.View;
 import com.mojang.datafixers.types.Type;
 import com.mojang.datafixers.types.families.Algebra;
+import com.mojang.datafixers.types.families.ListAlgebra;
 import com.mojang.datafixers.types.families.RecursiveTypeFamily;
 import com.mojang.datafixers.types.templates.RecursivePoint;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.DynamicOps;
 import org.apache.commons.lang3.tuple.Triple;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 
@@ -38,6 +42,33 @@ final class Fold<A, B> extends PointFree<Function<A, B>> {
     @Override
     public Type<Function<A, B>> type() {
         return DSL.func(aType, bType);
+    }
+
+    @Override
+    Optional<? extends PointFree<Function<A, B>>> all(final PointFreeRule rule) {
+        final int familySize = aType.family().size();
+        final List<RewriteResult<?, ?>> newAlgebra = new ArrayList<>(familySize);
+        boolean changed = false;
+        for (int i = 0; i < familySize; i++) {
+            final RewriteResult<?, ?> view = algebra.apply(i);
+            final PointFree<? extends Function<?, ?>> function = view.view().function();
+            final PointFree<? extends Function<?, ?>> rewrite = rule.rewriteOrNop(function);
+            if (rewrite != function) {
+                newAlgebra.add(cap(view, rewrite));
+                changed = true;
+            } else {
+                newAlgebra.add(view);
+            }
+        }
+        if (changed) {
+            return Optional.of(new Fold<>(aType, bType, new ListAlgebra("Rewrite all", newAlgebra), index));
+        }
+        return Optional.empty();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <A, B> RewriteResult<A, B> cap(final RewriteResult<A, B> view, final PointFree<? extends Function<?, ?>> rewrite) {
+        return RewriteResult.create(new View<>((PointFree<Function<A, B>>) rewrite), view.recData());
     }
 
     private <FB> PointFree<Function<A, B>> cap(final RewriteResult<?, FB> resResult) {
