f7df0ad36d8b55362140120375560199f700856e
Gegy <gegy.dev@gmail.com>
2023-02-21 10:56:13 +0100

    Pass TypedOptic into ProfunctorTransformer

--- src/main/java/com/mojang/datafixers/optics/Optic.java
+++ src/main/java/com/mojang/datafixers/optics/Optic.java
@@ -8,7 +8,6 @@ import com.mojang.datafixers.kinds.App2;
 import com.mojang.datafixers.kinds.K1;
 import com.mojang.datafixers.kinds.K2;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
