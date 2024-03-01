f9e6e9bceb2e5b01ba3299c85b04bc57b71fb6ec
ThatGravyBoat <thatgravyboat@gmail.com>
2024-03-01 15:33:18 -0330

    Handle dispatch codec receiving null decoders more gracefully

--- src/main/java/com/mojang/serialization/Codec.java
+++ src/main/java/com/mojang/serialization/Codec.java
@@ -326,11 +326,23 @@ public interface Codec<A> extends Encoder<A>, Decoder<A> {
     }
 
     default <E> Codec<E> dispatch(final String typeKey, final Function<? super E, ? extends A> type, final Function<? super A, ? extends Codec<? extends E>> codec) {
-        return partialDispatch(typeKey, type.andThen(DataResult::success), codec.andThen(DataResult::success));
+        return partialDispatch(typeKey, type.andThen(DataResult::success), key -> {
+            final Codec<? extends E> result = codec.apply(key);
+            if (result != null) {
+                return DataResult.success(result);
+            }
+            return DataResult.error(() -> "No codec found for key: " + key);
+        });
     }
 
     default <E> Codec<E> dispatchStable(final Function<? super E, ? extends A> type, final Function<? super A, ? extends Codec<? extends E>> codec) {
-        return partialDispatch("type", e -> DataResult.success(type.apply(e), Lifecycle.stable()), a -> DataResult.success(codec.apply(a), Lifecycle.stable()));
+        return partialDispatch("type", e -> DataResult.success(type.apply(e), Lifecycle.stable()), key -> {
+            final Codec<? extends E> result = codec.apply(key);
+            if (result != null) {
+                return DataResult.success(result, Lifecycle.stable());
+            }
+            return DataResult.error(() -> "No codec found for key: " + key, Lifecycle.stable());
+        });
     }
 
     default <E> Codec<E> partialDispatch(final String typeKey, final Function<? super E, ? extends DataResult<? extends A>> type, final Function<? super A, ? extends DataResult<? extends Codec<? extends E>>> codec) {
@@ -342,7 +354,13 @@ public interface Codec<A> extends Encoder<A>, Decoder<A> {
     }
 
     default <E> MapCodec<E> dispatchMap(final String typeKey, final Function<? super E, ? extends A> type, final Function<? super A, ? extends Codec<? extends E>> codec) {
-        return new KeyDispatchCodec<>(typeKey, this, type.andThen(DataResult::success), codec.andThen(DataResult::success));
+        return new KeyDispatchCodec<>(typeKey, this, type.andThen(DataResult::success), (key) -> {
+            final Codec<? extends E> result = codec.apply(key);
+            if (result != null) {
+                return DataResult.success(result);
+            }
+            return DataResult.error(() -> "No map codec found for key: " + key);
+        });
     }
 
     // private
