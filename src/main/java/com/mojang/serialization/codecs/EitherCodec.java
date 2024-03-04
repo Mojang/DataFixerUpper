0e553293eb13aa08bac6980943e38fbaee6ebf22
Georgii Gavrichev (fry) <fry@mojang.com>
2024-03-04 18:40:09 +0100

    Improve error messaging for EitherCodec

--- src/main/java/com/mojang/serialization/codecs/EitherCodec.java
+++ src/main/java/com/mojang/serialization/codecs/EitherCodec.java
@@ -8,24 +8,18 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
 
-import java.util.Objects;
-
-public final class EitherCodec<F, S> implements Codec<Either<F, S>> {
-    private final Codec<F> first;
-    private final Codec<S> second;
-
-    public EitherCodec(final Codec<F> first, final Codec<S> second) {
-        this.first = first;
-        this.second = second;
-    }
-
+public record EitherCodec<F, S>(Codec<F> first, Codec<S> second) implements Codec<Either<F, S>> {
     @Override
     public <T> DataResult<Pair<Either<F, S>, T>> decode(final DynamicOps<T> ops, final T input) {
         final DataResult<Pair<Either<F, S>, T>> firstRead = first.decode(ops, input).map(vo -> vo.mapFirst(Either::left));
-        if (firstRead.result().isPresent()) {
+        if (firstRead.error().isEmpty()) {
             return firstRead;
         }
-        return second.decode(ops, input).map(vo -> vo.mapFirst(Either::right));
+        final DataResult<Pair<Either<F, S>, T>> secondRead = second.decode(ops, input).map(vo -> vo.mapFirst(Either::right));
+        if (secondRead.error().isEmpty()) {
+            return secondRead;
+        }
+        return firstRead.apply2((f, s) -> s, secondRead);
     }
 
     @Override
@@ -34,27 +28,5 @@ public final class EitherCodec<F, S> implements Codec<Either<F, S>> {
             value1 -> first.encode(value1, ops, prefix),
             value2 -> second.encode(value2, ops, prefix)
         );
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final EitherCodec<?, ?> eitherCodec = ((EitherCodec<?, ?>) o);
-        return Objects.equals(first, eitherCodec.first) && Objects.equals(second, eitherCodec.second);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(first, second);
-    }
-
-    @Override
-    public String toString() {
-        return "EitherCodec[" + first + ", " + second + ']';
     }
 }
